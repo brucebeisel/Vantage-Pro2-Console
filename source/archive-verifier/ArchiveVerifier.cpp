24f1537acdc56d307332bfe6e92cdef833adb6ec
Bruce Beisel <brucebeisel@users.noreply.github.com>
2024-08-30 20:20:19 -0400

    Fixed syntax error from API change

--- source/archive-verifier/ArchiveVerifier.cpp
+++ source/archive-verifier/ArchiveVerifier.cpp
@@ -13,7 +13,7 @@ int
 main(int argc, char *argv[]) {
 
     if (argc != 2) {
-        cerr << "Usage: archive-verifier <archive directory>" << endl;
+        cerr << "Usage: archive-verifier <archive file>" << endl;
         exit(1);
     }
 
@@ -23,6 +23,6 @@ main(int argc, char *argv[]) {
     VantageWeatherStation station(port);
     ArchiveManager mgr(archiveDir, station);
 
-    mgr.verifyArchiveFile();
+    mgr.verifyCurrentArchiveFile();
 
 }
