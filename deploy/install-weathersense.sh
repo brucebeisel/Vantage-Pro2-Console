31fca67f28c7e32857a9f25ca4597e4ce76e8c44
Weathersense <brucebeisel@gmail.com>
2024-09-24 20:35:41 -0400

    Added copy of old archives to new install

--- deploy/install-weathersense.sh
+++ deploy/install-weathersense.sh
@@ -12,6 +12,18 @@ dir=$1
 
 version=`cat $dir/weathersense-version.txt`
 
+id -u weathersense &> /dev/null
+
+if [ $? == 1 ]; then
+    echo "User \"weathersense\" must exist to install WeatherSense $version"
+    exit 2
+fi
+
+if [ -d /weathersense ]; then
+    echo "Directory /weathersense must exist to install WeatherSense $version"
+    exit 3
+fi
+
 mkdir /weathersense/$version
 
 cp -r $dir/* /weathersense/$version
@@ -24,3 +36,33 @@ chmod +x /etc/init.d/weathersense
 ln -s /etc/init.d/weathersense /etc/rc5.d/S99weathersense
 ln -s /etc/init.d/weathersense /etc/rc5.d/K99weathersense
 
+archives=`ls -r /weathersense/*/archive`
+
+numarchives=`wc -w <<< $archives`
+
+copyarchive=""
+
+if [ $numarchives -gt 0 ]; then
+    echo "A number of existing archives were found in the /weathersense directory"
+    echo "Would you like to copy the existing archive files to the new installation?"
+    read -p "[yes/no]: " answer
+    if [ $answer != "yes" ]; then
+        exit 4
+    fi
+    echo "The choices are (newest first): "
+    cat <<< $archives
+    echo "Choose one of the following files by entering "y" at the prompt"
+
+    for archive in $archives
+    do
+        read -p "$archive " answer
+        if [ $answer == "y" ]; then
+            copyarchive=$archive
+            break
+        fi
+    done
+fi
+
+if [ $copyarchive != "" ]; then
+    cp -r $copyarchive/* /weathersense/$version/archive
+fi
