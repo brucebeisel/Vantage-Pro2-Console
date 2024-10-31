9d2eea9b3b1370207f9140da795f94c434749748
Bruce Beisel <brucebeisel@users.noreply.github.com>
2024-10-31 14:31:52 -0400

    Updated verifier tool for new ArchiveManager API

--- source/archive-verifier/ArchiveVerifier.cpp
+++ source/archive-verifier/ArchiveVerifier.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <filesystem>
 #include "ArchiveManager.h"
 #include "VantageWeatherStation.h"
 #include "SerialPort.h"
@@ -13,24 +14,40 @@ int
 main(int argc, char *argv[]) {
 
     if (argc < 2) {
-        cerr << "Usage: archive-verifier <archive directory> [archive file]" << endl;
+        cerr << "Usage: archive-verifier <archive file>" << endl;
         exit(1);
     }
 
-    const char * archiveDir = argv[1];
-    char * archiveFile = NULL;
-    if (argc == 3)
-        archiveFile = argv[2];
+    if (!filesystem::exists(argv[1])) {
+        cerr << "Archive file '" << argv[1] << "' does not exists" << endl;
+        cerr << "Usage: archive-verifier <archive file>" << endl;
+        exit(1);
+    }
 
+    const char * archiveDir = NULL;
+    const char * archiveFile = NULL;
+
+    if (filesystem::is_directory(argv[1])) {
+        cerr << "Specified file '" << argv[1] << "' is a directory not a file." << endl;
+        exit(1);
+    }
+
+    filesystem::path path(argv[1]);
+    if (!path.has_parent_path()) {
+        cerr << "Directory not found in path '" << argv[1] << "' Using '.' as archive directory" << endl;
+        archiveDir = ".";
+    }
+    else {
+        archiveDir = path.parent_path().c_str();
+    }
+
+    archiveFile = path.filename().c_str();
 
     SerialPort port("/dev/tty", 9600);
     VantageWeatherStation station(port);
-    ArchiveManager *mgr;
-    if (archiveFile == NULL)
-        mgr = new ArchiveManager(archiveDir, station);
-    else
-        mgr = new ArchiveManager(archiveDir, archiveFile, station);
 
-    mgr->verifyCurrentArchiveFile();
+    ArchiveManager mgr(archiveDir, archiveFile, station);
+
+    mgr.verifyArchiveFile(argv[1]);
 
 }
