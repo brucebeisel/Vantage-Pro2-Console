3155d4bd3bf329c27e3c4262d050c78fe45ee5e0
Bruce Beisel <brucebeisel@users.noreply.github.com>
2024-12-18 19:52:17 -0500

    Changed copyright to 2025

--- source/vws/VantageConfiguration.cpp
+++ source/vws/VantageConfiguration.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Bruce Beisel
+ * Copyright (C) 2025 Bruce Beisel
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
