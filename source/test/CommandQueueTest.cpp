19567f8912df6b090aaadeea06411dfb36420432
Bruce Beisel <brucebeisel@users.noreply.github.com>
2025-05-25 15:34:20 -0400

    Updated to use new version of CommandData

--- source/test/CommandQueueTest.cpp
+++ source/test/CommandQueueTest.cpp
@@ -19,32 +19,46 @@
 #include <thread>
 #include <unistd.h>
 
+#include "VantageLogger.h"
 #include "CommandQueue.h"
 #include "CommandData.h"
 
 using namespace std;
 using namespace vws;
 
-CommandQueue commandQueue;
+CommandQueue * commandQueue;
 bool terminateThread = false;
+int commandCount = 0;
 
 void
-consumerThread() {
-    cout << "Started consumer thread" << endl;
+waitForThread() {
+    cout << "Started waitFor thread" << endl;
 
     CommandData commandData;
-    while (commandQueue.waitForCommand(commandData))
-        cout << "Command 1: " << commandData << endl;
+    while (!terminateThread) {
+        if (commandQueue->waitForCommand(commandData)) {
+            commandCount++;
+            cout << "Command " << commandCount << ": " << commandData << endl;
+        }
+    }
 
-    cout << "Leaving waitForCommand(), entering consumeCommand()" << endl;
+}
+
+void
+consumeThread() {
+    cout << "Started consume thread" << endl;
+
+    CommandData commandData;
 
     while (!terminateThread) {
-        if (!commandQueue.isCommandAvailable()) {
+        if (!commandQueue->isCommandAvailable()) {
             cout << "No commands in queue" << endl;
         }
 
-        if (commandQueue.consumeCommand(commandData))
+        if (commandQueue->consumeCommand(commandData)) {
             cout << "Command 2: " << commandData << endl;
+            commandCount++;
+        }
         else
             cout << "No command found in queue" << endl;
 
@@ -55,31 +69,46 @@ consumerThread() {
 
 int
 main(int argc, char *argv[]) {
+    commandQueue = new CommandQueue();
 
-    thread t(consumerThread);
 
-    sleep(2);
+    VantageLogger::setLogLevel(VantageLogger::VANTAGE_DEBUG3);
 
+    //
+    // Queue two commands then start the thread. The waitForCommand() loop should consume both
+    // commands.
+    //
     CommandData commandData;
     commandData.commandName = "Command1";
     commandData.arguments.push_back(CommandData::CommandArgument("name", "value"));
-    commandData.fd = 100;
+    commandData.socketId = 100;
     commandData.response = "";
     commandData.responseHandler = NULL;
 
-    commandQueue.queueCommand(commandData);
-    sleep(1);
-    commandQueue.interrupt();
+    commandQueue->queueCommand(commandData);
+    commandData.socketId = 101;
+    commandQueue->queueCommand(commandData);
 
-    sleep(5);
+    thread t(waitForThread);
 
+    sleep(2);
+
+    if (commandCount == 2)
+    cout << "PASSED: CommandQueue offered 2 commands" << endl;
+    else
+    cout << "FAILED: CommandQueue offered " << commandCount << " commands, instead of 2" << endl;
+
+    terminateThread = true;
+    commandQueue->interrupt();
+    t.join();
+
+    /*
     commandData.arguments.push_back(CommandData::CommandArgument("name2", "value2"));
-    commandQueue.queueCommand(commandData);
+    commandQueue->queueCommand(commandData);
 
     sleep(2);
 
     terminateThread = true;
     t.join();
-
-
+    */
 }
